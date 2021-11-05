f2fb7c008fc4725498af06c03bc1e85567a5d789
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-11-05 14:32:08 -0500

    add User Posts to User Profile

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,11 +1,19 @@
 import { Profile } from "../models/profile.js"
+import { Post } from "../models/post.js"
 
 function show(req, res) {
   Profile.findById(req.params.id)
+  .populate('posts')
   .then(profile => {
-    res.render('profiles/show', {
-      title: 'Show Profile',
-      profile
+    Post.find({owner: req.params.id})
+    .populate('owner')
+    // profile.posts.populate('owner')
+    .then(posts => {
+      res.render('profiles/show', {
+        title: 'Show Profile',
+        profile,
+        posts: posts
+      })
     })
   })
   .catch(err => {
